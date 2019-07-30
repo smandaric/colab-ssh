891c9780b406b7154505ee7b5b7448fc6a3caed9
Stefan Mandaric <stefan.mandaric@unacast.com>
2019-07-30 14:07:25 +0200

    add setup

---
+++ colabssh.py
@@ -0,0 +1,32 @@
+import os
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials, ApplicationDefaultCredentialsError
+
+def copy_keys(drive_folder='ssh'):
+    # Create ssh folder
+    if not os.path.exists('/root/.ssh'):
+        os.makedirs('/root/.ssh')
+
+    # Authenticate Google Drive
+    gauth = GoogleAuth()
+
+    try:
+    gauth.credentials = GoogleCredentials.get_application_default()
+    except ApplicationDefaultCredentialsError:
+    auth.authenticate_user()
+    gauth.credentials = GoogleCredentials.get_application_default()
+
+    drive = GoogleDrive(gauth)
+    
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false and mimeType='application/vnd.google-apps.folder' and title='ssh'"}).GetList()
+    assert len(file_list) == 1
+    
+    ssh_folder_id = file_list[0]['id']
+    ssh_file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(ssh_folder_id)}).GetList()
+
+    for ssh_file in ssh_file_list:
+    ssh_file.GetContentFile('/root/.ssh/{}'.format(ssh_file['title']))
+    print('Copied file /root/.ssh/{}'.format(ssh_file['title']))
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colabssh",
+    version="0.0.1",
+    author="Stefan Mandaric",
+    author_email="stefan.mandaric@gmail.com",
+    description="Copy SSH keys from Google Drive to Colaboratory",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smandaric/colabssh",
+    packages=['PyDrive']
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
