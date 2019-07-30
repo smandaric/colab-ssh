98772e6d90df8ed17ae4d369d0e4f720e2933572
Stefan Mandaric <stefan.mandaric@unacast.com>
2019-07-30 14:36:20 +0200

    indentation fix

--- colabssh.py
+++ colabssh.py
@@ -14,19 +14,20 @@ def copy_keys(drive_folder='ssh'):
     gauth = GoogleAuth()
 
     try:
-    gauth.credentials = GoogleCredentials.get_application_default()
+        gauth.credentials = GoogleCredentials.get_application_default()
     except ApplicationDefaultCredentialsError:
-    auth.authenticate_user()
-    gauth.credentials = GoogleCredentials.get_application_default()
+        auth.authenticate_user()
+        gauth.credentials = GoogleCredentials.get_application_default()
 
     drive = GoogleDrive(gauth)
     
-    file_list = drive.ListFile({'q': "'root' in parents and trashed=false and mimeType='application/vnd.google-apps.folder' and title='ssh'"}).GetList()
+    #Copy all files in Drive folder
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false and mimeType='application/vnd.google-apps.folder' and title='{}'".format(drive_folder)}).GetList()
     assert len(file_list) == 1
     
     ssh_folder_id = file_list[0]['id']
     ssh_file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(ssh_folder_id)}).GetList()
 
     for ssh_file in ssh_file_list:
-    ssh_file.GetContentFile('/root/.ssh/{}'.format(ssh_file['title']))
-    print('Copied file /root/.ssh/{}'.format(ssh_file['title']))
+        ssh_file.GetContentFile('/root/.ssh/{}'.format(ssh_file['title']))
+        print('Copied file /root/.ssh/{}'.format(ssh_file['title']))
